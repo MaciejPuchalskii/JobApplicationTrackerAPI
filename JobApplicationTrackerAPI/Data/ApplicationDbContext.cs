9b288f20bdbefaff126a8bd2c5e784edbe9f23e8
Maciej Puchalski <maciejpe34@gmail.com>
2025-02-10 13:26:02 +0100

    Create DbContext with Identity

---
+++ JobApplicationTrackerAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using JobApplicationTrackerAPI.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobApplicationTrackerAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<JobApplication>()
+                        .HasOne(j => j.User)
+                        .WithMany(u => u.JobApplications)
+                        .HasForeignKey(j => j.UserId)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<JobApplication>()
+                        .HasOne(j => j.Company)
+                        .WithMany(c => c.JobApplications)
+                        .HasForeignKey(j => j.CompanyId)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Attachment>()
+                        .HasOne<JobApplication>()
+                        .WithMany(j => j.Attachments)
+                        .HasForeignKey(a => a.JobApplicationId)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Note>()
+                        .HasOne<JobApplication>()
+                        .WithMany(j => j.NotesList)
+                        .HasForeignKey(n => n.JobApplicationId)
+                        .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
