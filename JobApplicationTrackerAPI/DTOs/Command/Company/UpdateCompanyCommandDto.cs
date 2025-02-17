598f7fef93ee684b285d9d9680b423ac8b6b6611
Maciej Puchalski <maciejpe34@gmail.com>
2025-02-17 15:20:02 +0100

    Remove DataAnnotations validation in Dto class

--- JobApplicationTrackerAPI/DTOs/Command/Company/UpdateCompanyCommandDto.cs
+++ JobApplicationTrackerAPI/DTOs/Command/Company/UpdateCompanyCommandDto.cs
@@ -1,18 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace JobApplicationTrackerAPI.DTOs.Command.Company
+﻿namespace JobApplicationTrackerAPI.DTOs.Command.Company
 {
     public class UpdateCompanyCommandDto
     {
-        [StringLength(200, ErrorMessage = "Description cannot exceed 200 characters")]
         public string? Description { get; set; }
-
         public string? ContactPerson { get; set; }
-
-        [Phone]
         public string? PhoneNumber { get; set; }
-
-        [Url]
         public string? CompanyURL { get; set; }
     }
 }
